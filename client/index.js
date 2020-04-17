9fece04173cd49dbc16a24ee0ce5f64c0573baee
Sonica1031 <Sonica1031@hotmail.com>
2020-04-16 23:32:30 -0500

    server

--- client/index.js
+++ client/index.js
@@ -13,3 +13,55 @@ server.listen(port, hostname, () => {
   // start watching for connections on the port specified
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+server.get("/edibles", (req, res) =>{
+  res.status(200).send(
+    {
+        id: 1,
+        imageSRC: '../images/gummi.jpeg',
+        description: "Gummi",
+        qty: 0
+    },
+    {
+        id: 2,
+        imageSRC: '../images/gummi.jpeg',
+        description: "Gummi",
+        qty: 0
+    },
+    {
+        id: 3,
+        imageSRC: '../images/gummi.jpeg',
+        description: "Gummi",
+        qty: 0
+    },
+    {
+        id: 4,
+        imageSRC: '../images/gummi.jpeg',
+        description: "Gummi",
+        qty: 0
+    },
+    {
+        id: 5,
+        imageSRC: '../images/gummi.jpeg',
+        description: "Gummi",
+    },
+    {
+        id: 6,
+        imageSRC: '../images/gummi.jpeg',
+        description: "Gummi",
+        qty: 0
+    }
+  )
+})
+
+server.post('/edibles', (req, res) => {
+  res.status(201).json({ url: '/edibles', operation: 'POST' });
+});
+
+server.put('/edibles', (req, res) => {
+  res.status(200).json({ url: '/edibles', operation: 'PUT' });
+});
+
+server.delete('/edibles', (req, res) => {
+  res.status(204);
+});
