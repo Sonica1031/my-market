dd5a285f0ed963d089478046dedf670bbcc03622
Sonica1031 <53800242+Sonica1031@users.noreply.github.com>
2020-04-19 18:45:08 -0500

    Add files via upload

---
+++ src/components/Leaves.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { leaf } from '../data';
+import { Link } from 'react-router-dom';
+import dryleaves from '../images/dryleaves.jpeg'
+
+const Leaves = (props) =>{
+    return(
+ <div className="edible-container">
+    {leaf.map(item =>{
+        return(
+            <Link className="aTagForEdibles" path to={`/leaves/${item.id}`}>
+            <div className="columns">
+                <p>
+                    {item.description}
+                </p>
+                <img src={dryleaves} width="500px"/>
+            </div>
+            </Link>
+        )
+    })}
+      </div>
+    )
+}
+
+export default Leaves;
