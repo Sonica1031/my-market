0a55581e9664021663a3f1e82a14574d500d2db9
Sonica1031 <Sonica1031@hotmail.com>
2020-04-20 15:59:50 -0500

    fix

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD:src/components/cart.js
-import React, { useEffect } from 'react';
-import Axios from 'axios';
 
-export const Cart = () =>{
-useEffect(() => {
-    Axios
-        .get()
-},[])
-    return(
-        <div>
-        </div>
-    );
-=======
 import React from 'react';
 
 export const Cart = () =>{
@@ -19,5 +6,4 @@ export const Cart = () =>{
         <div>
         </div>
     );
->>>>>>> a0cabce86e3031a4662f994c80137920b1aaee89:src/components/Cart.js
 }
