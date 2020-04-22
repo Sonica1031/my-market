c7f283368b6c71c06c1d3794f20d9d53a22a7290
Sonica1031 <Sonica1031@hotmail.com>
2020-04-21 21:06:30 -0500

    new logo

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -41,7 +41,7 @@ const [selling, setSelling] = useState([])
                 })}
                 <p className="aTagForCart">Price: {selling.reduce((total, num) => {
                   return total + num.price * num.qty;
-                },0)}.00</p>
+                },0)}</p>
         </div>
     );
 }
