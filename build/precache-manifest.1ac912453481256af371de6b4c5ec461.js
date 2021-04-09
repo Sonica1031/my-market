08903dbd64197c067b619b75692b0ccd52e7b22d
Sonica1031 <Sonica1031@hotmail.com>
2021-04-08 22:05:06 -0500

    Upgrade

---
+++ build/precache-manifest.1ac912453481256af371de6b4c5ec461.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c802dd2c2f4dd659f4743fddb5529d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7639bc0e739cd907b78d",
+    "url": "/static/css/main.449a11ce.chunk.css"
+  },
+  {
+    "revision": "0e27545475cb978a29da",
+    "url": "/static/js/2.c538a86d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c538a86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7639bc0e739cd907b78d",
+    "url": "/static/js/main.2119c453.chunk.js"
+  },
+  {
+    "revision": "4afca21f37c38a1052bc",
+    "url": "/static/js/runtime-main.1c564db4.js"
+  },
+  {
+    "revision": "aab5a5514d581f08285f1a3bf6f1364d",
+    "url": "/static/media/Dark Mode.aab5a551.png"
+  },
+  {
+    "revision": "ae85d6844bbee56d0aad4c7c56379274",
+    "url": "/static/media/Puff Palace.ae85d684.png"
+  },
+  {
+    "revision": "f7df56c1b577514d21f08fd7d681f27c",
+    "url": "/static/media/button-cart.f7df56c1.png"
+  },
+  {
+    "revision": "86af8ad07436c00e74d1e3d631e8d993",
+    "url": "/static/media/darkmode-cart.86af8ad0.png"
+  },
+  {
+    "revision": "c9d0fdc70ae267ef2783fe697b5705cb",
+    "url": "/static/media/dryleaves.c9d0fdc7.jpeg"
+  },
+  {
+    "revision": "746626876102e7f1e6a92657d6c9513f",
+    "url": "/static/media/gummi.74662687.jpeg"
+  }
+]);
