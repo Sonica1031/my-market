461f07433c85bb3d481efe58bc0d8966df35522d
Sonica1031 <sonica1031@hotmail.com>
2025-09-07 15:39:22 -0500

    Accidentally deleted items from products page:

--- src/components/Bows.jsx
+++ src/components/Bows.jsx
@@ -20,7 +20,11 @@ return(
         <div className={`bows-container ${darkMode ? 'dark-mode-black-background' : ''}`}>
             {bowItems.map(item => (
                 <Link key={item.id} className='aTagForBows' to={`/products/${item.id}`}>
-                    
+                      <div className="columns">
+                            <p>{item.title}</p>
+                            <img src={item.image} width="150" alt={item.description} />
+                            <p>Price: {item.price}</p>
+                        </div>
                 </Link>
             ))} 
         </div>
